cec36fd0412bc257d6ca9a34caad67542a174e9a
Steven O <steveno@liatrio.com>
2019-10-03 15:03:06 -0700

    cleanup, added error stage

--- src/main/java/io/jenkins/plugins/MyGraphListener.java
+++ src/main/java/io/jenkins/plugins/MyGraphListener.java
@@ -1,6 +1,7 @@
 package io.jenkins.plugins;
 
 import hudson.Extension;
+import org.jenkinsci.plugins.workflow.actions.ErrorAction;
 import org.jenkinsci.plugins.workflow.actions.LabelAction;
 import org.jenkinsci.plugins.workflow.actions.StageAction;
 import org.jenkinsci.plugins.workflow.actions.ThreadNameAction;
@@ -38,9 +39,19 @@ public class MyGraphListener implements GraphListener {
         }
 
         if (flowNode.getClass() == StepEndNode.class && Ids.contains(((StepEndNode) flowNode).getStartNode().getId())) {
+            ErrorAction errorAction = flowNode.getError();
+            if(errorAction != null){
+                log.info("");
+                log.info("### Error Info ###");
+                log.warning("Error Action: " + errorAction);
+                log.warning("Error: " + errorAction.getError());
+                log.warning("Error Display Name: " + errorAction.getDisplayName());
+                log.info("### /Error Info ###");
+                log.info("");
+            }
+
             StepEndNode endNode = (StepEndNode) flowNode;
             BlockStartNode startNode = endNode.getStartNode();
-
             log.info("");
             log.info("### Ending Stage for " +
                     ((StepEndNode) flowNode).getStartNode().getDisplayName() +
