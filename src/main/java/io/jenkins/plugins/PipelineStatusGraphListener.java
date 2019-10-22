b49ed847b30b947811bed6ae09aa07cd28350b70
Ahmed Alsabag <ahmedalsabag@Ahmeds-MBP.home>
2019-10-22 14:01:45 -0500

    working event push

--- src/main/java/io/jenkins/plugins/PipelineStatusGraphListener.java
+++ src/main/java/io/jenkins/plugins/PipelineStatusGraphListener.java
@@ -1,5 +1,6 @@
 package io.jenkins.plugins;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import hudson.Extension;
 import org.jenkinsci.plugins.workflow.actions.ErrorAction;
 import org.jenkinsci.plugins.workflow.cps.nodes.StepEndNode;
@@ -13,8 +14,10 @@ import io.kubernetes.client.*;
 import io.kubernetes.client.apis.CoreV1Api;
 import io.kubernetes.client.models.*;
 import io.kubernetes.client.util.ClientBuilder;
+import io.kubernetes.client.apis.CustomObjectsApi;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -35,13 +38,13 @@ public class PipelineStatusGraphListener implements GraphListener {
 
     // the CoreV1Api loads default api-client from global configuration.
         CoreV1Api api = new CoreV1Api();
-
+        CustomObjectsApi apiInstance = new CustomObjectsApi();
     // invokes the CoreV1Api client
         V1PodList list = api.listPodForAllNamespaces(null, null, null, null, null, null, null, null, null);
         for (V1Pod item : list.getItems()) {
             log.info(item.getMetadata().getName());
         }
-    } catch (Exception exception) { log.info("exception caught");}
+
     
 
         if (flowNode.getClass() == StepStartNode.class) {
@@ -76,6 +79,33 @@ public class PipelineStatusGraphListener implements GraphListener {
             log.info("Display Name: '" + endNode.getDisplayName() + "'");
             log.info("### /Ending Stage ###");
             log.info("");
+            log.info("trying to build event");
+            String group = "stable.liatr.io"; // String | The custom resource's group name
+            String version = "v1"; // String | The custom resource's version
+            String namespace = "default"; // String | The custom resource's namespace
+            String plural = "logs"; // String | The custom resource's plural name. For TPRs this would be lowercase plural kind.
+            String json = "{\n" +
+                    "  \"apiVersion\": \"stable.liatr.io/v1\",\n" +
+                    "  \"kind\": \"JenkinsLog\",\n" +
+                    "  \"metadata\": {\n" +
+                    "    \"name\": \"event3\"\n" +
+                    "  },\n" +
+                    "  \"spec\": {\n" +
+                    "    \"logMessage\": \"Stage 3 complete\"\n" +
+                    "  }\n" +
+                    "}"; // Object | The JSON schema of the Resource to create.
+            HashMap<String,Object> body =
+                    new ObjectMapper().readValue(json, HashMap.class);
+            String pretty = "true"; // String | If 'true', then the output is pretty printed.
+
+            Object result = apiInstance.createNamespacedCustomObject(group, version, namespace, plural, body, pretty);
+            log.info((String)result);
+            log.info("finished building event");
+
         }
+    } catch (Exception exception) {
+        log.info("exception caught");
+        log.info(exception.toString());
+    }
     }
 }
